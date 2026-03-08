c08594ce356c0057ec3d509a4bc4963e7a58a555
David <p5y4@laposte.net>
2026-03-08 10:49:32 +0100

    prisma

--- routes/csrfRoutes.js
+++ routes/csrfRoutes.js
@@ -8,7 +8,6 @@ const csrfProtection = csurf();
 // ENDPOINT POUR OBTENIR UN TOKEN CSRF //
 
 router.get('/', csrfProtection, (req, res) => {
-    console.log('CSRF failed:', err);
     res.json({ csrfToken: req.csrfToken() });
 });
 
